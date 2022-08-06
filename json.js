0fd17afd34100a56bb4051f83ddf354c17f3529a
jose <carlosmendesbgi@gmail.com>
2022-08-06 14:45:32 -0300

    arquivo Json

---
+++ json.js
@@ -0,0 +1,17 @@
+let filmes = [{
+    nome: "Um Contratempo",
+    anoLancamento: 2016,
+    genero: "Thriller/Mistério",
+    duracao: 106,
+    diretor: "Oriol Paulo"
+},
+{
+    nome: "O Poderoso Chefão",
+    anoLancamento: 1972,
+    genero: "Crime/Drama",
+    duracao: 175,
+    diretor: " Francis Ford Coppola"
+}]
+
+let jsonFilmes = JSON.stringify(filmes)
+console.log(jsonFilmes)
